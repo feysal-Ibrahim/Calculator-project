416e6b7fd947353033661be9e49fad68094cbaf7
first-name second-name <iamfeysal@gmail.com>
2018-04-26 11:55:56 +0300

    added link to the script file

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var add = function(number1, number2) {
+      return number1 + number2;
+    };
+     alert(add(10,5));
+      var add = function(number1, number2) {
+        return number1 + number2;
+      };
+ var add = function(number1, number2) {
+        return number1 + number2;
+      };
+    var add = function(number1, number2) {
+        return number1 + number2;
+      };
+​
+      var number1 =parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another number:"));
+​
+      alert(add(number1, number2));
