a5b9c8e916c7fcb521d190affcb5d6a5192579fe
first-name second-name <iamfeysal@gmail.com>
2018-04-26 15:02:40 +0300

    added more text to script file

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,7 @@
-var add = function(number1, number2) {
-      return number1 + number2;
-    };
-     alert(add(10,5));
-      var add = function(number1, number2) {
-        return number1 + number2;
-      };
- var add = function(number1, number2) {
-        return number1 + number2;
-      };
-    var add = function(number1, number2) {
-        return number1 + number2;
-      };
-​
-      var number1 =parseInt(prompt("Enter a number:"));
-      var number2 = parseInt(prompt("Enter another number:"));
-​
-      alert(add(number1, number2));
+
+var a = parseFloat(prompt("Enter Num 1"))
+var b = parseFloat(prompt("Enter Num 2"))
+
+var result= a+b
+
+alert(results)
